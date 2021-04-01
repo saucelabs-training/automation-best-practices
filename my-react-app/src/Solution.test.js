250515da0d5eaedabf7a259703f67d24fae92a1d
nadvolod <nadvolod@gmail.com>
2021-04-01 15:32:54 -0400

    solution added

---
+++ my-react-app/src/Solution.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+//Using Jest matchers: https://jestjs.io/docs/using-matchers
+
+test('renders learn react link', () => {
+  //render our App component in a virtual DOM
+  render(<App />);
+  //search for an element by text
+  const linkElement = screen.getByTestId('learn-link');
+  //expect this element to be present in the HTML
+  expect(linkElement).toBeInTheDocument();
+})
+
+test('link has correct url', () => {
+  //render our App component in a virtual DOM
+  render(<App />);
+  //search for an element by text
+  const linkElement = screen.getByTestId('learn-link');
+  //Using Jest matchers: https://jestjs.io/docs/using-matchers
+  expect(linkElement.href).toContain('https://reactjs.org');
+})
+
+test('link opens in new tab', () => {
+  //render our App component in a virtual DOM
+  render(<App />);
+  //search for an element by text
+  const linkElement = screen.getByTestId('learn-link');
+  //Link should open a new tab
+  expect(linkElement.target).toBe('_blank')
+})
