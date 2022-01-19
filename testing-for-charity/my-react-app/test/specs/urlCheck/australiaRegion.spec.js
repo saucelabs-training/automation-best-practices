955ebb494e5426c673dcb78fdbafc720cf23a1d7
nadvolod <nadvolod@gmail.com>
2022-01-19 12:39:07 -0500

    minor cleanup to get to a better state

--- testing-for-charity/my-react-app/test/specs/urlCheck/australiaRegion.spec.js
+++ testing-for-charity/my-react-app/test/specs/urlCheck/australiaRegion.spec.js
@@ -2,15 +2,15 @@ var homePage = require('../../../pageObject/home.page');
 const { assert } = require('assert');
 
 describe('Australia region url check', () => {
-    before(async () => {
-        await homePage.open('australia');
-    });
+  before(async () => {
+    await homePage.open('australia');
+  });
 
-    it('HomePage check', async () => {
-        await homePage.initialize('Australia homePage check')
-        await homePage.closeCountryModal();
-        await homePage.makeScreenShot('Australia - HomePage');
-        const result = await homePage.resolve();
-        assert.ok(result.passed, result.message);
-    });
+  it('HomePage check', async () => {
+    await homePage.initialize('australia ');
+    await homePage.closeCountryModal();
+    await homePage.makeScreenShot('HomePage');
+    const result = await homePage.resolve();
+    assert.ok(result.passed, result.message);
+  });
 });
